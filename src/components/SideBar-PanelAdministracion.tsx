7ed72ab205a3dce38b8f84429828bce50b08431a
Jhonbuenovillarroel <jhonadelbuenovillarroel@gmail.com>
2023-11-02 01:07:58 -0500

    commit 65

--- src/components/SideBar-PanelAdministracion.tsx
+++ src/components/SideBar-PanelAdministracion.tsx
@@ -42,7 +42,6 @@ export default function SideBarPanelAdministracion() {
             color: "#fff",
             confirmButtonColor: "#CB993F",
          });
-         signOut();
 
          return;
       }
